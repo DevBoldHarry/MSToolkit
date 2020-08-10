709e3933108a4af2e7ce6500e2ee4c7d917c7e3e
harry.stewart <harry.stewart@boldcommerce.com>
2020-08-10 10:31:56 -0500

    Made additional changes: - Date format -> Change to Date: Hr:Min - Other Tickets by Shop -> Collapsable section - Other Tickets by Requester -> Collapsable section - Highlight the app if the support segment is high priority (Diamond/HVM)
    	modified:   assets/core.js
    	modified:   assets/iframe.html
    	modified:   assets/styles.css

--- assets/core.js
+++ assets/core.js
@@ -14,10 +14,85 @@ client.get(['ticket', 'ticket.customField:custom_field_23800156']).then(async (d
 
     let url = data['ticket.customField:custom_field_23800156'],
         ticketData = data['ticket'],
-        ticketNumber = ticketData.id;
+        ticketNumber = ticketData.id,
+        requesterEmail = ticketData.requester.email;
+
+    let highPriorityArray = ['seg-hvm', 'seg-managed_services', 'seg-ms_platinum', 'seg-ms_diamond'];
+    let isHighPriority = ticketData.tags.some(r=> highPriorityArray.includes(r));
+
+    let resultsData = {
+        urlResults: {},
+        requesterResults: {}
+    }
+
+    resultsData = {
+        urlResults: await getTicketsByUrl(url, ticketNumber),
+        requesterResults: await getTicketsByRequester(requesterEmail, ticketNumber),
+        isHighPriority: isHighPriority
+    }
+
+    showInfo(resultsData)
+})
+
+async function getTicketsByUrl(url, ticketNumber) {
+
+    let urlResults = {
+        tickets: {},
+        ratings: {}
+    };
+
+    if (url.includes('myshopify.com')) {
+
+        let ticketOptions = {
+            url: `/api/v2/search.json?query=status<=solved fieldvalue:${url}`,
+            type: 'GET'
+        };
+
+        // Request ticket search
+        let {results} = await client.request(ticketOptions);
+
+        // Calculate satisfaction percentage from results
+        let ratings = calculatePerc(results);
+
+        // Find Group Name for each ticket in list
+        let tickets = Object.values(results);
+        let newLength = tickets.length;
+
+        for (let i=0; i<tickets.length; i++){
+            let ticket = tickets[i];
+
+            if (ticket.id === ticketNumber){
+                delete tickets[i];
+                newLength--;
+                continue;
+            }
+
+            ticket.group_name = await getGroups(ticket);
+            ticket.updated_at = formatDate(ticket.updated_at);
+        }
+
+        tickets.newLength = newLength;
+
+        urlResults = {
+            tickets: tickets,
+            ratings: ratings
+        }
+
+    }
+
+    return urlResults;
+
+}
+
+async function getTicketsByRequester(requesterEmail, ticketNumber) {
+    
+    let requesterResults = {
+        tickets: {},
+        ratings: {}
+    };
 
     let ticketOptions = {
-        url: `/api/v2/search.json?query=status<=solved fieldvalue:${url}`,
+        url: `/api/v2/search.json?query=status<=solved requester:${requesterEmail}`,
         type: 'GET'
     };
 
@@ -29,33 +104,41 @@ client.get(['ticket', 'ticket.customField:custom_field_23800156']).then(async (d
 
     // Find Group Name for each ticket in list
     let tickets = Object.values(results);
-    for(let i=0; i<tickets.length; i++){
-        let groupOptions = {
-            url: `/api/v2/groups/${tickets[i].group_id}.json`,
-            type: 'GET'
-        }
-        let {group} = await client.request(groupOptions);
-        tickets[i].group_name = group.name;
+    let newLength = tickets.length;
 
-        // Add property if IDs match
-        if(ticketNumber === tickets[i].id)
-            tickets[i].currentTicket = true;
+    for (let i=0; i<tickets.length; i++){
+        let ticket = tickets[i];
+
+        if (ticket.id === ticketNumber){
+            delete tickets[i];
+            newLength--;
+            continue;
+        }
+
+        ticket.group_name = await getGroups(ticket);
+        ticket.updated_at = formatDate(ticket.updated_at);
     }
 
-    const resultsData = {
+    tickets.newLength = newLength;
+
+    requesterResults = {
         tickets: tickets,
         ratings: ratings
     }
 
-    showInfo(resultsData)
-})
+    return requesterResults;
+}
 
-function showInfo(requester_data) {
-    let source = $("#requester-template").html();
-    let template = Handlebars.compile(source);
-    let html = template(requester_data);
-    $("#content").html(html);
-  }
+async function getGroups(ticket) {
+
+    let groupOptions = {
+        url: `/api/v2/groups/${ticket.group_id}.json`,
+        type: 'GET'
+    }
+
+    let {group} = await client.request(groupOptions);
+    return group.name;
+}
 
 function calculatePerc(tickets) {
     let posNum = 0,
@@ -88,7 +171,14 @@ function calculatePerc(tickets) {
     return result;
 }
 
-// Register Handlebars if greater than operator (if a > b)
+function formatDate(date) {
+    let current_datetime = new Date(date)
+    let formatted_date = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " (" + current_datetime.getHours() + ":" + ( current_datetime.getMinutes() < 10 ? '0' : '' ) + current_datetime.getMinutes() + ")";
+    return formatted_date;
+}
+
+// Handlebars Functions //
+
 Handlebars.registerHelper('if_greater', function(a, b, opts) {
     if (a > b) {
         return opts.fn(this);
@@ -96,3 +186,18 @@ Handlebars.registerHelper('if_greater', function(a, b, opts) {
         return opts.inverse(this);
     }
 });
+
+function showInfo(requester_data) {
+    let source = $("#requester-template").html();
+    let template = Handlebars.compile(source);
+    let html = template(requester_data);
+    $("#content").html(html);
+}
+
+// Collapse Function //
+
+function toggleFunction() {
+    let collapse = event.currentTarget.parentElement.querySelector('.app-section[data-collapsed]')
+    let isTrueSet = (collapse.getAttribute('data-collapsed') == 'true');
+    collapse.setAttribute('data-collapsed', !isTrueSet);
+}
